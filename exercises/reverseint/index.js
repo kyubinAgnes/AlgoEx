36375675436525fbd303a1194f909b3610f6d8c3
K <inkblot626@gmail.com>
2023-07-16 14:56:05 +0900

    Integer Reversal

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,11 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const sign = Math.sign(n);
+    let reversedArray = parseInt(n.toString().split('').reverse().join(''));
+
+    return reversedArray * sign
+}
 
 module.exports = reverseInt;
