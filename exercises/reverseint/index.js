5822606174eb48bfa8f5b46eb68c60ba3ce9dad1
K <inkblot626@gmail.com>
2023-07-16 15:01:09 +0900

    Integer Reversal final

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -9,7 +9,8 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-    return parseInt(n.toString().split('').reverse().join('')) * Math.sign(n)
+    const reversed = n.toString().split('').reverse().join('');
+    return parseInt(reversed) * Math.sign(n)
 }
 
 module.exports = reverseInt;
