bcf847a8e042b22a8d8ed2eefa4c417232ba3a68
K <inkblot626@gmail.com>
2023-07-16 14:00:05 +0900

    Palindromes 2

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,8 +8,15 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-    const reversed = str.split("").reverse().join("")
-    return reversed === str
+    return str.split('').every((char, i) => {
+        return char === str[str.length - i - 1]
+    })
 }
 
 module.exports = palindrome;
+
+
+// function palindrome(str) {
+//     const reversed = str.split("").reverse().join("")
+//     return reversed === str
+// }
