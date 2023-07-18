2a38087abc8242f1244adb8b2a21c57e1fa30968
K <inkblot626@gmail.com>
2023-07-18 10:23:18 +0900

    FizzBuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -13,8 +13,8 @@
 //   buzz
 
 function fizzBuzz(n) {
-    for(let i = 1; i < n + 1; i++) {
-        if(i % 3 === 0 && i % 5 === 0) {
+    for(let i = 1; i <= n; i++) {
+        if(i % 15 === 0) {
             console.log("fizzbuzz")
         } else if(i % 3 === 0) {
             console.log("fizz")
