d45b986628b15c3d7c8fbac0663d57337b997bda
K <inkblot626@gmail.com>
2023-07-26 18:56:54 +0900

    Capitalize 1

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,11 +8,21 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-  const words = str.split(" ");
-  const newArray = words.map((item) => {
-    return item[0].toUpperCase() + item.slice(1);
-  });
-  return newArray.join(" ");
+  const words = [];
+
+  for (let word of str.split(" ")) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  return words.join(" ");
 }
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//     const words = str.split(" ");
+//     const newArray = words.map((item) => {
+//       return item[0].toUpperCase() + item.slice(1);
+//     });
+//     return newArray.join(" ");
+//   }
