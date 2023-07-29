543a985d800d722e410cba9a15718765f312d509
K <inkblot626@gmail.com>
2023-07-29 13:01:57 +0900

    Vowels - my solution

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,15 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let vowels = ["a", "e", "i", "o", "u"];
+  let count = 0;
+  for (const i of str.toLowerCase()) {
+    if (vowels.includes(i)) {
+      count += 1;
+    }
+  }
+  return count;
+}
 
 module.exports = vowels;
