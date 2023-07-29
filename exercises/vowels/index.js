face3c52c911c5475c1c4c7170610b919b78c5a0
K <inkblot626@gmail.com>
2023-07-29 13:06:22 +0900

    Vowels 1

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,14 +8,28 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-  let vowels = ["a", "e", "i", "o", "u"];
   let count = 0;
-  for (const i of str.toLowerCase()) {
-    if (vowels.includes(i)) {
-      count += 1;
+  //   const checker = "aeiou";
+  const checker = ["a", "e", "i", "o", "u"];
+
+  for (let char of str.toLowerCase()) {
+    if (checker.includes(char)) {
+      count++;
     }
   }
+
   return count;
 }
 
 module.exports = vowels;
+
+// function vowels(str) {
+//     let vowels = ["a", "e", "i", "o", "u"];
+//     let count = 0;
+//     for (const i of str.toLowerCase()) {
+//       if (vowels.includes(i)) {
+//         count += 1;
+//       }
+//     }
+//     return count;
+//   }
