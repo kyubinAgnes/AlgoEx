0f03f8064fd1f3fdcf66b166e98f91cfb1a589e7
K <inkblot626@gmail.com>
2023-08-26 23:26:10 +0900

    insertAt - my solution

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -114,6 +114,25 @@ class LinkedList {
     }
     previous.next = previous.next.next;
   }
+
+  insertAt(data, index) {
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+
+    const previous = this.getAt(index - 1);
+    if (!previous) {
+      const node = this.getLast();
+      node.next = new Node(data);
+      return;
+    }
+
+    const node = new Node(data);
+
+    node.next = previous.next;
+    previous.next = node;
+  }
 }
 
 module.exports = { Node, LinkedList };
