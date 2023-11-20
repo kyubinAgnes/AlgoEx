7a1e9de5cfc7d67e82a235f951cc3124e1a57cbd
inkblot626 <inkblot626@gmail.com>
2023-11-20 16:27:33 +0900

    sorting - bubble - mysolution2

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,9 +2,15 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-  arr.sort(function (a, b) {
-    return a - b;
-  });
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
   return arr;
 }
 
